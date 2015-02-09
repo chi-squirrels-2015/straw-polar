580e2eba81891edeb45a2c037ee2a1e3235ab4a3
John N. Mandalakas & Matthew DeCarlo <pair+jnmandal.matthewwho@devbootcamp.com>
2015-02-09 15:55:54 -0600

    add skeleton of routes to controllers

---
+++ app/controllers/polls.rb
@@ -0,0 +1,19 @@
+get "/polls/new" do
+
+end
+
+get "/polls/:id" do
+
+end
+
+post "/polls" do
+
+end
+
+put "/polls/:id" do
+  
+end
+
+delete "/polls/:id" do
+
+end
---
+++ app/controllers/questions.rb
@@ -0,0 +1,19 @@
+get "/polls/:poll_id/questions/new" do
+
+end
+
+get "/polls/:poll_id/questions/:id" do
+
+end
+
+post "/polls/questions" do
+
+end
+
+put "/polls/:poll_id/questions/:id" do
+  
+end
+
+delete "/polls/:poll_id/questions/:id" do
+
+end
